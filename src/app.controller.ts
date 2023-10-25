30dfd1e2a44c049c32e6d0d96a022d168fe71184
Avtandil Saparov <avtanndil@gmail.com>
2023-10-25 17:22:14 +0300

    install prisma && create user schema

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaClient } from '@prisma/client';
+import { ApiOkResponse, ApiProperty } from '@nestjs/swagger';
+const prisma = new PrismaClient();
+
+class HelloWorldDto {
+  @ApiProperty()
+  message: string;
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOkResponse({
+    type: HelloWorldDto,
+  })
+  async getHello(): Promise<HelloWorldDto> {
+    const users = prisma.user.findMany({});
+    console.log(users);
+    return { message: this.appService.getHello() };
   }
 }
